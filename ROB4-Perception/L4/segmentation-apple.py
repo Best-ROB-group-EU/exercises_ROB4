5211a711aa18d7e09268949ccf875d597f89411b
frederikjchristensen <frederikjchristensen@gmail.com>
2020-02-27 12:22:08 +0100

    Finished exercises

--- ROB4-Perception/L4/segmentation-apple.py
+++ ROB4-Perception/L4/segmentation-apple.py
@@ -10,14 +10,14 @@ hsv = cv2.cvtColor(Source, cv2.COLOR_BGR2HSV)
 leaf = cv2.inRange(hsv, (36, 25, 25), (70, 255,255))
 
 # Isolate the stalk
-
 stalk = cv2.inRange(Source,(20,20,80),(100,100,150))
 
 # Masking by colour (Finding apple)
 apple = cv2.inRange(Source, (0, 0, 50), (200, 200, 255))
 
-# Subtracting the leaf
+# Subtracting the leaf and stalk
 mask = cv2.subtract(apple,leaf)
+#mask = cv2.subtract(mask,stalk)
 
 # Performing Opening
 # Create a dot shaped kernel
@@ -28,6 +28,10 @@ opening = cv2.dilate(erosion,kernel,1)
 # Perform Closing and bring back the apple
 kernel2 = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (25, 25))
 dilation = cv2.dilate(opening,kernel,1)
+closing = cv2.erode(dilation,kernel,1)
+
+#Create a comparison
+comparison = Source & cv2.cvtColor(closing, cv2.COLOR_GRAY2BGR)
 
 # Show images
 cv2.imshow("Source",Source)
@@ -38,8 +42,8 @@ cv2.imshow("Apple", apple)
 cv2.imshow("Mask", mask)
 cv2.imshow("Opening", opening)
 
-cv2.imshow("Final", dilation)
-
+cv2.imshow("Final", closing)
+cv2.imshow("Comparison", comparison)
 # Quit program is 'q' is pressed
 if cv2.waitKey(0) & 0xFF == ord('q'):
     cv2.destroyAllWindows()
